34875c04ebc03a568f9bfc826c2029616a1e573d
Buta Yama <butayama@users.noreply.github.com>
2019-09-02 20:31:24 +0200

    sourcecode with included doctest code for functions in a .py file.
    The file ran correct from console:
    E:\GitHub\Python-Testprogramme>python -m doctest Zahlenraten.py
    
    It also ran correct within PyCharm with the doctest option activated

--- Zahlenraten.py
+++ Zahlenraten.py
@@ -1,15 +1,47 @@
-geheimnis = 1337
+"""This module is an example for a while loop and if query to find a secret number
+
+written during a VHS python course in Bochum.
+Now I test the pydoc tool with this code
+
+Author: Uwe Schweinsberg, Bochum
+License: GNU GPL v 2
+Date: 2. 9. 2019
+
+functions: evaluate_number
+"""
+
+
+# import sys
+
+
+def evaluate_number(n, sn):
+    """evaluate_number compares two integers
+    the first number is compared to the second.
+    If the first number is smaller or bigger than the second number
+    this information is printd. If the two numbers are equal there is no output.
+    >>> evaluate_number(3, 5)
+    Zu klein
+
+    >>> evaluate_number(5, 3)
+    Zu groß
+
+    >>> evaluate_number(3, 3)
+
+    ''
+    """
+    if n < sn:
+        print("Zu klein")
+    if n > sn:
+        print("Zu groß")
+
+
+# geheimnis = int(sys.argv[1])
+geheimnis = 1052
 versuch = 0
 zaehler = 0
 while versuch != geheimnis:
     versuch = int(input("Rate Zahl: "))
-    if versuch < geheimnis:
-        print("Zu klein")
-
-    if versuch > geheimnis:
-        print("Zu groß")
-
+    evaluate_number(versuch, geheimnis)
     zaehler += 1
 print("in ", zaehler, "Versuchen")
 
-    
